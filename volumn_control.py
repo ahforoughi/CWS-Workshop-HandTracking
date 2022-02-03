1bb7bcb606e7ef712bf6cbe6dcfe5b05ddd46069
Moxitron <70322560+moxitron@users.noreply.github.com>
2022-02-03 04:16:50 +0330

    adding some comment

--- volumn_control.py
+++ volumn_control.py
@@ -2,7 +2,7 @@ import cv2
 import time 
 import HandTrackingModule as htm
 
-# changing volumn accordingly - only works on linux ! 
+# this method changes system volumn accordingly - only works on linux ! 
 from subprocess import call 
 def set_volumn(percentage) : 
     call(["amixer", "-D", "pulse", "sset", "Master", str(percentage)+"%"])
@@ -17,14 +17,15 @@ while True :
 	sec , img = cap.read() 
 	img = detector.findHands(img , True) 
 	lm = detector.findPosition(img) 
-
+	
+	# check if hand was detected 
 	if len(lm) != 0 : 
 		length = detector.findDistance(4 , 8 , img , True)[0] 
 		vol_per = length / 350 * 100 
 		set_volumn(vol_per)
 		cv2.rectangle(img , (50 , 100) , (85 , 400) , (0 , 255 , 0) , 3)
 		cv2.rectangle(img , (50 , 400 - int(vol_per / 100 * (400 - 100) )) , (85 , 400) , (0 , 255 , 0) , cv2.FILLED)
-		cv2.putText(img , "Vol : " + str(int(vol_per)) , (50, 450) , cv2.FONT_HERSHEY_COMPLEX , 0.5 , (0 , 0 , 255) , 1)		
+		cv2.putText(img , "Vol : " + str(int(vol_per)) + "%" , (50, 450) , cv2.FONT_HERSHEY_COMPLEX , 0.5 , (0 , 0 , 255) , 1)		
 
 	curr_time = time.time()
 	fps = 1 / (curr_time - prev_time) 
